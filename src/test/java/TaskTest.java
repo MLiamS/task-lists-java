fca7f028d688eee1bc107abc43babce155a7b4be
Dallas Slaughter and liam stabeno <dallas@>
2017-03-21 15:41:39 -0700

    Followed lessons, built to-do list

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -3,4 +3,30 @@ import static org.junit.Assert.*;
 
 public class TaskTest {
 
+  @Test
+  public void Task_instantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
+  @Test
+public void clear_emptiesAllTasksFromArrayList_0() {
+  Task myTask = new Task("Mow the lawn");
+  Task.clear();
+  assertEquals(0, Task.all().size());
+}
+
 }
